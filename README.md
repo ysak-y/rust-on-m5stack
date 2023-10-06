29711d90092ba63d0ee3f2b24fbc49fa4fdb0c13
yoshiaki-yamada <yoshiaki-yamada@cookpad.com>
2023-10-06 18:55:40 +0900

    Implement display example

--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 Rust examples for M5Stack.
 To implement and flash firmware for M5Stack in Rust, please check following tools at first.
 
+I use [M5Stack GRAY](https://docs.m5stack.com/en/core/gray) to run firmware, maybe you need to modify something to run correctly if you want to run it on other devices.
+
 ## espup
 
 This firmware is witten in Rust. You need to install `espup`. first. Please follow this document https://github.com/esp-rs/rust-build .
@@ -15,3 +17,7 @@ You can write firmware by using [espflash](https://github.com/esp-rs/espflash).
 $ cargo build
 $ espflash flash /target/xtensa-esp-espidf/debug/your-project-name
 ```
+
+## Examples
+
+- [Display](./display/)
--- display/src/main.rs
+++ display/src/main.rs
@@ -1,12 +1,77 @@
-use esp_idf_sys as _; // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
-use log::*;
+use display_interface_spi::SPIInterfaceNoCS;
+use embedded_graphics::mono_font::ascii::FONT_6X10;
+use embedded_graphics::{
+    mono_font::MonoTextStyle,
+    pixelcolor::Rgb565,
+    prelude::*,
+    text::{Alignment, Text},
+};
+use esp_idf_hal::gpio::PinDriver;
+use esp_idf_hal::prelude::*;
+use esp_idf_hal::spi::{config, SpiDeviceDriver, SpiDriver, SpiDriverConfig, SPI2};
+use mipidsi::Builder;
+use std::{thread, time::Duration};
 
 fn main() {
-    // It is necessary to call this function once. Otherwise some patches to the runtime
-    // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
-    esp_idf_sys::link_patches();
-    // Bind the log crate to the ESP Logging facilities
-    esp_idf_svc::log::EspLogger::initialize_default();
+    let peripherals = esp_idf_hal::peripherals::Peripherals::take().unwrap();
+    let gpios = peripherals.pins;
 
-    info!("Hello, world!");
+    let pin_dc = PinDriver::output(gpios.gpio27).unwrap();
+    let mut lcd_reset_pin = PinDriver::output(gpios.gpio33).unwrap();
+
+    // Initialize back light of the display
+    let mut pin_lcd_blk = PinDriver::output(gpios.gpio32).unwrap();
+    pin_lcd_blk.set_high().unwrap();
+
+    // Issue LCD Reset
+    lcd_reset_pin.set_low().unwrap();
+    thread::sleep(Duration::from_millis(100));
+    lcd_reset_pin.set_high().unwrap();
+    thread::sleep(Duration::from_millis(2000));
+
+    // Initialize SPI interface
+    let spi = peripherals.spi2;
+    let driver = SpiDriver::new::<SPI2>(
+        spi,
+        gpios.gpio18,
+        gpios.gpio23,
+        Some(gpios.gpio19),
+        &SpiDriverConfig::new(),
+    )
+    .unwrap();
+
+    let spi_device_config = config::Config::new().baudrate(10.MHz().into());
+    let spi_device = SpiDeviceDriver::new(driver, Some(gpios.gpio14), &spi_device_config).unwrap();
+
+    // Initialize Display Interface
+    let display_interface = SPIInterfaceNoCS::new(spi_device, pin_dc);
+
+    // Initialize ILI9342c that is the M5Stack screen.
+    let mut display = Builder::ili9342c_rgb565(display_interface)
+        .with_display_size(320, 240)
+        .with_color_order(mipidsi::ColorOrder::Bgr)
+        .init(&mut esp_idf_hal::delay::FreeRtos, Some(lcd_reset_pin))
+        .unwrap();
+
+    // Draw 5x5 rectangle at position (0, 0).
+    display
+        .set_pixels(
+            0,
+            0,
+            5,
+            5,
+            core::iter::repeat(Rgb565::RED).take(25).into_iter(),
+        )
+        .unwrap();
+
+    // Draw text at position (50, 50)
+    let style = MonoTextStyle::new(&FONT_6X10, Rgb565::RED);
+    Text::with_alignment(
+        "Hello World!!",
+        Point::new(50, 50),
+        style,
+        Alignment::Center,
+    )
+    .draw(&mut display)
+    .unwrap();
 }
