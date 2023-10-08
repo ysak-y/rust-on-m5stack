613464346cf549a6a68f8caa10323cad492e9aa8
ysak-y <yoshiaki.0614@gmail.com>
2023-10-09 02:00:35 +0900

    Refactor button sample

--- button/src/main.rs
+++ button/src/main.rs
@@ -1,6 +1,19 @@
 use esp_idf_hal::gpio::PinDriver;
 use std::{thread, time::Duration};
 
+macro_rules! process_button {
+    ($pin:expr, $prev_state:expr, $button_name:expr) => {
+        // Print text if button A is low (= pressed) and was not low (= not pressed) in the previous iteration.
+        if $pin.is_low() && !$prev_state {
+            println!("Button {} pressed", $button_name);
+            $prev_state = true;
+        // Set prev_btn_a_is_low to false if button A is high (= not pressed) and was low (= pressed) in the previous iteration.
+        } else if $pin.is_high() && $prev_state {
+            $prev_state = false;
+        }
+    };
+}
+
 fn main() {
     let peripherals: esp_idf_hal::peripherals::Peripherals =
         esp_idf_hal::peripherals::Peripherals::take().unwrap();
@@ -15,35 +28,9 @@ fn main() {
     let mut prev_btn_c_is_low = false;
 
     loop {
-        // Print text if button A is low (= pressed) and was not low (= not pressed) in the previous iteration.
-        if pin_btn_a.is_low() && !prev_btn_a_is_low {
-            println!("Button A pressed");
-            prev_btn_a_is_low = true;
-        }
-
-        // Set prev_btn_a_is_low to false if button A is high (= not pressed) and was low (= pressed) in the previous iteration.
-        if pin_btn_a.is_high() && prev_btn_a_is_low {
-            prev_btn_a_is_low = false;
-        }
-
-        // Implement same logics for button B and C.
-        if pin_btn_b.is_low() && !prev_btn_b_is_low {
-            println!("Button B pressed");
-            prev_btn_b_is_low = true;
-        }
-
-        if pin_btn_b.is_high() && prev_btn_b_is_low {
-            prev_btn_b_is_low = false;
-        }
-
-        if pin_btn_c.is_low() && !prev_btn_c_is_low {
-            println!("Button C pressed");
-            prev_btn_c_is_low = true;
-        }
-
-        if pin_btn_c.is_high() && prev_btn_c_is_low {
-            prev_btn_c_is_low = false;
-        }
+        process_button!(pin_btn_a, prev_btn_a_is_low, "A");
+        process_button!(pin_btn_b, prev_btn_b_is_low, "B");
+        process_button!(pin_btn_c, prev_btn_c_is_low, "C");
 
         thread::sleep(Duration::from_millis(10));
     }
